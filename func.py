a64cdfeef93839196fbf30120e8aa5c97321e052
caroline-swanton <cswanton@andrew.cmu.edu>
2025-02-08 17:48:33 -0500

    update main.py
    final

--- func.py
+++ func.py
@@ -5,7 +5,6 @@ import mediapipe as mp
 import HandTrackingModule as htm
 from cvzone.HandTrackingModule import HandDetector
 
-
 def distance_between_wrists(left_list,right_list):
     temp_x = (left_list[0][0] - right_list[0][0])**2
     temp_y = (left_list[0][1] - right_list[0][1])**2
@@ -25,7 +24,6 @@ def detect_fly(left_hand,right_hand):
             return True
     return False
 
-
 def detect_throw(hand,zList):
     hand_list = hand["lmList"]
     base_i_x=hand_list[8][0]
@@ -37,49 +35,33 @@ def detect_throw(hand,zList):
 
     delta_distance = (zList[-1] - zList[0]) // len(zList)
 
-
-
     if (len(zList)>=10):
         zList.pop(0)
     if delta_distance >=30:
         return True
     return False
   
-
-
 def fist_palm_tip_help(left_list,right_list):
     index_dif = abs(left_list[8][1]-right_list[8][1])
     middle_dif = abs(left_list[12][1]-right_list[12][1])
     ring_dif = abs(left_list[16][1]-right_list[16][1])
     pinky_dif = abs(left_list[20][1]-right_list[20][1])
 
-
-
     return index_dif>=200 and middle_dif>=200 and ring_dif>=200 and pinky_dif>=200
-
 
 def detect_fist_palm(left_hand,right_hand):
     left_hand_list = left_hand["lmList"]
     right_hand_list = right_hand["lmList"]
-
-
     index_knuckle_dif = ((left_hand_list[5][0]- right_hand_list[5][0])**2 + (left_hand_list[5][1]- right_hand_list[5][1])**2) ** 0.5 
     middle_knuckle_dif = ((left_hand_list[9][0]- right_hand_list[9][0])**2 + (left_hand_list[9][1]- right_hand_list[9][1])**2) ** 0.5 
     ring_knuckle_dif = ((left_hand_list[13][0]- right_hand_list[13][0])**2 + (left_hand_list[13][1]- right_hand_list[13][1])**2) ** 0.5 
     pinky_knuckle_dif = ((left_hand_list[17][0]- right_hand_list[17][0])**2 + (left_hand_list[17][1]- right_hand_list[17][1])**2) ** 0.5 
-
-
-    
-
-
-
 
     if(index_knuckle_dif<200 and middle_knuckle_dif<200 and ring_knuckle_dif<200 and pinky_knuckle_dif<200):
         if(fist_palm_tip_help(left_hand_list,right_hand_list)):
             return True
         return False
     return False
-
 
 def detect_dragonBall(left_hand, right_hand):
     left_hand_list = left_hand["lmList"]
@@ -103,9 +85,6 @@ def detect_dragonBall(left_hand, right_hand):
     avgDx = (dxRing + dxPinky + dxMid + dxIndex) / 4
     return avgDy >= 500 and avgDx <= 200 and wristDist <= 200
 
-
-
-
 def detect_triangle(left_hand,right_hand):
     left_hand_list = left_hand["lmList"]
     right_hand_list = right_hand["lmList"]
@@ -118,14 +97,7 @@ def detect_triangle(left_hand,right_hand):
     thumb_avg_y =  (left_hand_list[4][1] + right_hand_list[4][1])//2
     thumb_avg_x =  (left_hand_list[4][1] + right_hand_list[4][1])//2
 
-
-
-
-
     return thumb_distance<150 and index_distance<150 and abs(index_avg_y -thumb_avg_y) > 350 and abs(index_avg_x -thumb_avg_x)<500
-
-
-
 
 def detect_catch(hand,rev_t_list):
     hand_list = hand["lmList"]
@@ -138,7 +110,6 @@ def detect_catch(hand,rev_t_list):
 
     delta_distance_rev = (rev_t_list[-1] - rev_t_list[0]) // len(rev_t_list)
 
-
     if (len(rev_t_list)>=5):
         rev_t_list.pop(0)
 
@@ -146,8 +117,6 @@ def detect_catch(hand,rev_t_list):
     if delta_distance_rev <=-25:
         return True
     return False
-
-
 
 def detect_swipe(hand,handX):
     hand_list = hand["lmList"]
@@ -157,20 +126,12 @@ def detect_swipe(hand,handX):
 
     delta_middle_x = (handX[-1] - handX[0]) // len(handX)
 
-
     if(len(handX)>=10): 
         handX.pop(0)
 
     if(abs(delta_middle_x)>=75):
         return True 
     return False
-
-
-# def detect
-
-
-
-
 
 def detect_mouse(hand,list_of_fingers,wCam,hCam,wScr,hScr):
     hand_List = hand["lmList"]
--- tech.py
+++ tech.py
@@ -2,14 +2,23 @@ import cv2
 import pygame
 import numpy as np
 import mediapipe as mp
-import random
+import pygame
+import app
+from screens import *
+from trainer import *
+from player_mon import *
+from opponent_mon import *
+from text_box import *
+from main import *
+
 
 import HandTrackingModule as htm
 from cvzone.HandTrackingModule import HandDetector
 
-from fly import *
+from func import *
 
-
+last_update = pygame.time.get_ticks() 
+clock = pygame.time.Clock()
 # Initialize Pygame
 pygame.init()
 
@@ -79,7 +88,6 @@ just_caught = True
 
 
 def display_mouse(coordinates, color):
-    screen.fill((0,0,0))
     x,y = coordinates
     pygame.draw.circle(screen,color,(x,y),10,2)
 
@@ -92,6 +100,54 @@ pygame.mouse.set_pos(mouseX, mouseY)
 # delay  = - 1
 wait = 5
 while running:
+    screen.fill((0,0,0))
+    app.canvas.blit(app.bg, (0,0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False 
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if app.in_start and pressedStart(event.pos):
+                toGuide()
+            elif (app.in_win or app.in_lose) and pressedPlayAgain(event.pos):
+                toStart()
+                app.reset()
+            elif app.in_guide and pressedGotIt(event.pos):
+                toSelect()
+            elif app.in_select:
+                if pressedBack(event.pos):
+                    toStart()
+                if hoverBulb(event.pos):
+                    if not(app.bulbDead):
+                        toBattle()
+                        if not(app.pickBulb):
+                            app.trainer = Trainer()
+                        app.pickChar = False
+                        app.pickBulb = True
+                        app.pickSquirt = False
+                        app.curr_pokemon_dead = False
+                elif hoverChar(event.pos): 
+                    if not(app.charDead):
+                        toBattle()
+                        if not(app.pickChar):
+                            app.trainer = Trainer()
+                        app.pickChar = True
+                        app.pickBulb = False
+                        app.pickSquirt = False
+                        app.curr_pokemon_dead = False
+                elif hoverSquirt(event.pos): 
+                    if not(app.squirtDead):
+                        toBattle()
+                        if not(app.pickSquirt):
+                            app.trainer = Trainer()
+                        app.pickChar = False
+                        app.pickBulb = False
+                        app.pickSquirt = True
+                        app.curr_pokemon_dead = False
+        elif event.type == pygame.KEYDOWN: #temporary for fainting pokemon and going to end screen
+            if event.key == pygame.K_r:
+                app.reset()
+                toStart()
     
     
     # Handle Pygame event
@@ -143,21 +199,21 @@ while running:
             isClick ,coord = detect_mouse(hand,detector.fingersUp(hand),target_width,target_height,screen_width,screen_height)
             # print(type(coord))
             if not isClick:
-                mousePressed = False
+                app.mousePressed = False
                 wait = 5
                 # print("COORD",coord)
                 # print("COORD00000", coord[0])
-                mouseX = coord[0]
+                app.mouseX = coord[0]
                 # print("MOUSE XXXXX", mouseX)
-                mouseY = coord[1]
+                app.mouseY = coord[1]
                 display_mouse(coord, 'white')
             else:
-                mouseX = coord[0]
-                mouseY = coord[1]
+                app.mouseX = coord[0]
+                app.mouseY = coord[1]
                 wait -= 1
                 if wait == 0:
-                    mousePressed = True
-                    display_mouse(coord, 'red')
+                    app.mousePressed = True
+                if app.in_start and pressedStart((app.mouseX, app.mouseY)):
             
             #pygame.mouse.set_pos(200,300)
             
@@ -259,7 +315,96 @@ while running:
     # Display sequence progress
     font = pygame.font.Font(None, 36)
     text = font.render(f"Current Move:{current_gesture}", True, (255, 255, 255))
-    screen.blit(text, (screen_width // 2 - 150, screen_height - 50))
+    #screen.blit(text, (screen_width // 2 - 150, screen_height - 50))
+
+    if app.in_battle:
+                if event.key == pygame.K_BACKSPACE:
+                    toSelect()
+                if app.curr_pokemon == None: # spawn in opponent and player
+                    if event.key == pygame.K_SPACE:
+                        app.trainer.throwing_ball = True
+                        throw_ball()
+                        spawn_player_mon()
+                        if app.opponent == None: spawn_opponent()
+                else:
+                    # player attacks
+                    if event.key in (pygame.K_1, pygame.K_2, pygame.K_3, pygame.K_4) and app.opponent:
+                        use_player_move(event)
+                    # opponent attacks
+                    elif event.key == pygame.K_h and app.curr_pokemon.hp > 0 and app.opponent != None:
+                        use_opponent_move()
+                    if app.hit_opponent_animation_active and app.opponent:
+                        hit_opponent()
+                    if app.hit_player_animation_active:
+                        hit_player()
+
+    if app.in_battle:
+        app.current_time = pygame.time.get_ticks()
+        
+        if app.trainer.showing:
+            throw_ball()
+            app.canvas.blit(app.trainer.animation[app.trainer.frame_index], (app.trainer.x, app.canvas_height - 300))
+
+        if app.curr_pokemon is not None: draw_player_mon()
+
+        if app.opponent is not None: draw_opponent_mon()
+
+        if app.text_box_active: draw_text_box()
+
+        if app.charDead and app.squirtDead and app.bulbDead: toLose()
+        elif app.opponent_dead: toWin()
+        elif app.curr_pokemon_dead: toSelect()
+
+    if app.in_select:
+        mousePos = pygame.mouse.get_pos()
+        instr_text, instr_rect = app.pokefont.render("CLICK TO CHOOSE", (0,0,0), size = 18)
+        instr_rect.center = (450, 535)
+        app.canvas.blit(instr_text, instr_rect)
+
+        if hoverBulb(mousePos):
+            app.canvas.blit(app.arrowImg, (140, 180))
+            if app.bulbDead:
+                warn_text, warn_rect = app.pokefont.render("FAINTED! CAN'T CHOOSE", (0,0,0), size = 18)
+                warn_rect.center = (450, 565)
+                app.canvas.blit(warn_text, warn_rect)
+        elif hoverChar(mousePos):
+            app.canvas.blit(app.arrowImg, (405, 180))
+            if app.charDead:
+                warn_text, warn_rect = app.pokefont.render("FAINTED! CAN'T CHOOSE", (0,0,0), size = 18)
+                warn_rect.center = (450, 565)
+                app.canvas.blit(warn_text, warn_rect)
+        elif hoverSquirt(mousePos):
+            app.canvas.blit(app.arrowImg, (680, 180))
+            if app.squirtDead:
+                warn_text, warn_rect = app.pokefont.render("FAINTED! CAN'T CHOOSE", (0,0,0), size = 18)
+                warn_rect.center = (450, 565)
+                app.canvas.blit(warn_text, warn_rect)
+                
+        else:
+            app.canvas.fill((0, 0, 0))
+            app.canvas.blit(app.bg, (0,0))
+
+        if app.bulbDead:
+            app.canvas.blit(app.bulbDeadImg, (88, 245))
+        else:
+            app.canvas.blit(app.bulbImg, (88, 245))
+        
+        if app.charDead:
+            app.canvas.blit(app.charDeadImg, (375, 240))
+        else: 
+            app.canvas.blit(app.charImg, (375, 240))
+        
+        if app.squirtDead:
+            app.canvas.blit(app.squirtDeadImg, (625, 240))
+        else:
+            app.canvas.blit(app.squirtImg, (625, 240))
+
+    if app.opponent_dead: pass
+
+    app.window.blit(app.canvas, (0,0))
+    screen.blit(frame, (0, 0))
+    pygame.display.update()
+    clock.tick(60)
 
 
     
